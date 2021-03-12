d1843ebe582809464b76b56f96055d90d6b4721e
Piotr Badełek <piotrbadelek@protonmail.com>
2021-03-12 11:10:06 +0100

    Add files via upload

---
+++ detect.js
@@ -0,0 +1,5 @@
+let h = document.createElement("script");
+h.textContent = `if (window.React || window.ReactDOM || document.querySelector('[data-reactroot], [data-reactid], #___gatsby, #__next')) {
+	document.body.innerHTML = \`<h1>This website was made with react, so it was deleted.</h1>\`;
+};`;
+document.body.appendChild(h);
