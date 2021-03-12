f647e311b1b5213c018ca64437de73683066d62a
Piotr Badełek <piotrbadelek@protonmail.com>
2021-03-12 12:10:55 +0100

    use strict

--- detect.js
+++ detect.js
@@ -1,3 +1,4 @@
+"use strict";
 let h = document.createElement("script");
 h.textContent = `if (window.React || window.ReactDOM || document.querySelector('[data-reactroot], [data-reactid], #___gatsby, #__next')) {
 	document.body.innerHTML = \`<h1>This website was made with react, so it was deleted.</h1>\`;
